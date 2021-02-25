9344e6651a71d996164492594a2653a342522263
Haris Michailidis <haris.michailidis@gmail.com>
2021-02-25 00:11:03 +0000

    1st POC from LSL to Kafka and read from Kafka

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import json
+from typing import List
+
+
+def sample_encoder(timestamp: float, sample: List[float]):
+    """
+
+    :param timestamp: float
+    :param sample: List[float]
+    :return: str(json)
+    """
+    sample_dict = dict([(i, s) for i, s in enumerate(sample)])
+    sample_dict['timestamp'] = timestamp
+    return json.dumps(sample_dict)
+
+
+def sample_decoder(encoded_sample: str):
+    """
+
+    :param encoded_sample: str
+    :return: int, List[float]
+    """
+    encoded_sample_dict = json.loads(encoded_sample)
+
+    timestamp = None
+    if "timestamp" in encoded_sample_dict.keys():
+        timestamp = encoded_sample_dict.pop('timestamp')
+
+    sample_keys = list(encoded_sample_dict.keys())
+    sample_keys.sort(reverse=False)
+
+    sample = []
+    for k in sample_keys:
+        sample.append(encoded_sample_dict[k])
+
+    return timestamp, sample
+
